88f227c1fd107b80e46ee87f4ae8d3539158b0ce
Marcello <marcello.topel@see.pb.gov.br>
2025-03-12 10:39:37 -0300

    adicao serializer e views

--- planejamento/settings.py
+++ planejamento/settings.py
@@ -42,6 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'planejamento',
+    'usuarios',
+    'setores',
+    'itens',
+    'planejamento_compras',
+    'celery'
 ]
 
 MIDDLEWARE = [
