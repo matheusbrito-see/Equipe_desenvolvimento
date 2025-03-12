db4b8e7f993f7d910edf0e21c8e7d987382355ea
Matheus <matheus.brito@see.pb.gov.br>
2025-03-12 11:09:01 -0300

    configuracao bootstrap no ambiente

--- planejamento/settings.py
+++ planejamento/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'planejamento.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
